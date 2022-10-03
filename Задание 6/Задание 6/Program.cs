dac86c90f19846ce04270c1a62a01765e42ef069
Godshri <108432016+Godshri@users.noreply.github.com>
2022-10-03 15:05:07 +0400

    Add files via upload

---
+++ Задание 6/Задание 6/Program.cs
@@ -0,0 +1,26 @@
+﻿double a;
+double b;
+double c;
+
+Console.WriteLine("Введите стороны треугольника");
+
+Console.Write("Сторона a: ");
+a = double.Parse(Console.ReadLine());
+
+Console.Write("Сторона b: ");
+b = double.Parse(Console.ReadLine());
+
+Console.Write("Сторона c: ");
+c = double.Parse(Console.ReadLine());
+
+if (a > b+c || b > a+c || c > a+b && a > 0 && b > 0 && c > 0)
+{
+    Console.WriteLine("Неверный ввод");
+    return;
+}
+
+double p = (a + b + c) / 2;
+double s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+
+Console.WriteLine($"\nПолупериметр: {p}");
+Console.WriteLine($"Площадь: {s}");
---
+++ Задание 7/Задание 7/Program.cs
@@ -0,0 +1,14 @@
+﻿byte x;
+byte y;
+
+Console.WriteLine("Введите числа");
+
+Console.Write("Первое число: ");
+x = byte.Parse(Console.ReadLine());
+
+Console.Write("Второе число: ");
+y =  byte.Parse(Console.ReadLine());
+
+Console.Write("Операция 'И'"); Console.WriteLine(x & y);
+Console.Write("Операция 'ИЛИ'"); Console.WriteLine(x | y);
+Console.Write("Операция 'Исключающее ИЛИ'"); Console.WriteLine(x ^ y);
