6f6fc09fd4e036eb1447a8363755e0f6f6c9796d
Godshri <108432016+Godshri@users.noreply.github.com>
2022-10-03 15:01:39 +0400

    Add files via upload

---
+++ Задание 1/Задание 1/Program.cs
@@ -0,0 +1,19 @@
+﻿double a;
+double b;
+
+Console.Write("Введите длину. A= ");
+
+a = double.Parse(Console.ReadLine());
+
+Console.Write("Введите ширину. В= ");
+b = double.Parse(Console.ReadLine());
+
+    if (a > 0 && b > 0)
+    {
+        Console.WriteLine("Длина: {0}, Ширина: {1}, Площадь: {2}", a, b, a * b);
+    }
+    else 
+    {
+        Console.WriteLine("Ошибка. Данные введены неправильно");
+        return;
+    }
---
+++ Задание 2/Задание 2/Program.cs
@@ -0,0 +1,15 @@
+﻿int size;
+int n = 1;
+
+Console.WriteLine("Введите число N");
+size = int.Parse(Console.ReadLine());
+
+while (n < size)
+{
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+    n++;
+}
